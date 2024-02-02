bdd3a42588cba13c011062452cb84028c2c97f9a
venkatsundaraj <venkateshsundarasan@gmail.com>
2024-02-02 11:46:12 +0530

    merge-commit

--- script.ts
+++ script.ts
@@ -8,6 +8,10 @@ const subractTen = function (value: number) {
 const multiply = function (value: number) {
   return value / 10
 }
+const sayHello = function () {
+  return "Hello world"
+}
 
 console.log(addTen(10))
 console.log(subractTen(10))
+console.log(sayHello())
